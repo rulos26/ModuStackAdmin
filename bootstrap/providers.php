5c3b6a5bd97c2b8fa4f8eb6a7eb799714a3e2292
juan.diaz <rulos26@gmail.com>
2025-11-02 21:32:20 -0500

    dele  version
    dele  version

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    Modules\Core\Providers\CoreServiceProvider::class,
-    Modules\Users\Providers\UsersServiceProvider::class,
-];
