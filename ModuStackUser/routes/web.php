83e4e65b7c1ce8c91ca8acff1891a2e07f03bcb7
juan.diaz <rulos26@gmail.com>
2025-11-09 18:38:35 -0500

    Refactor root route in web.php to use a closure for redirection to the login route, enhancing clarity and maintainability of route definitions.

--- ModuStackUser/routes/web.php
+++ ModuStackUser/routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/login');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
